7e588d2799755342c2a7929dc8021e555aa8ff36
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-04-30 09:43:11 +0530

    Add files via upload

---
+++ SpecialCount.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Scanner;
+public class SpecialCount {
+    
+ public static void main(String[] args) {
+Scanner scan= new Scanner(System.in);
+		String test = scan.nextLine();
+		count(test);
+
+	}
+	public static void count(String x){
+		char[] ch = x.toCharArray();
+		int letter = 0;
+		int space = 0;
+		int num = 0;
+		int other = 0;
+	System.out.println("input:");
+		for(int i = 0; i < x.length(); i++){
+			if(Character.isLetter(ch[i])){
+				letter ++ ;
+			}
+			else if(Character.isDigit(ch[i])){
+				num ++ ;
+			}
+			else if(Character.isSpaceChar(ch[i])){
+				space ++ ;
+			}
+			else{
+				other ++;
+			}
+		}
+		
+		System.out.println("output:" +other);
+		
+			}
+}
