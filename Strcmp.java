9354812c672c14ecf171e462374ca5f075a8ee92
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-05-09 11:28:20 +0530

    Add files via upload

---
+++ Strcmp.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.*;
+class Strcmp{  
+ public static void main(String args[]){  
+
+Scanner scan = new Scanner(System.in);
+   String s1=  scan.nextLine();
+   String s2=   scan.nextLine();
+
+if(s1.length() >= s2.length() )
+{
+ System.out.println(s1); 
+}
+else 
+{
+System.out.println(s2); 
+
+}
+ }  
+}  
