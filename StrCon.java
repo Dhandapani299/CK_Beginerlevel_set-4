a7271a228648dcca2d05115ed6082fed21ede34a
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-05-09 11:31:48 +0530

    Add files via upload

---
+++ StrCon.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.util.*;
+class StrCon{  
+ public static void main(String args[]){  
+
+Scanner scan = new Scanner(System.in);
+   String s1=  scan.nextLine();
+   String s2=   scan.nextLine();
+
+
+ System.out.println(s1+s2); 
+
+
+
+ }  
+}  
