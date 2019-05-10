a14dbb877ae66be85a047f76940dc35e4634b58c
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-05-10 12:17:10 +0530

    Add files via upload

---
+++ PowerOf.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.*;
+public class PowerOf {
+
+  
+
+  public static void main(String[] args) {
+System.out.println("input :");
+    Scanner in = new Scanner(System.in);
+    int number = in.nextInt();
+	
+    in.close();
+
+    if (isPowerOfTwo(number)) {
+      System.out.println("output: yes");
+    } else {
+      System.out.printf("output: no");
+    }
+  }
+
+  private static boolean isPowerOfTwo(int number) {
+
+    if (number % 2 != 0) {
+      return false;
+    } else {
+
+      for (int i = 0; i <= number; i++) {
+
+        if (Math.pow(2, i) == number) return true;
+      }
+    }
+    return false;
+  }
+}
