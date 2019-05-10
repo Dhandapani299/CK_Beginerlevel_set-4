b1f7f8a7e1500650928d5b3973f3bab6ab1fa742
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-05-10 13:27:09 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Print
+{
+     public static void main(String aa[])
+{
+   int a;
+    int n;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+
+     System.out.println(n);
+}}
