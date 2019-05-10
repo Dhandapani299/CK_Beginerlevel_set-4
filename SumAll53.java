47fee6819189fa0de58471837477bd057de1ba2e
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-05-10 15:49:41 +0530

    Add files via upload

---
+++ SumAll53.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+class SumAll53
+{
+   public static void main(String args[]){
+   int n,a,sum=0;
+   Scanner s = new Scanner(System.in);
+System.out.println("input:" );
+   n = s.nextInt();
+for(int i=0;i<n;i++){
+	a= s.nextInt();
+	sum = sum + a;
+	
+
+
+}System.out.println("output:" +sum);
+   
+
+
+
+
+
+      
+    }
+}
